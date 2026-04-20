d5068ef2b9cb9e6a14f52f4b165176582dd18b21
noahg17-game <noahg17@gmail.com>
2026-04-19 23:59:06 -0600

    gemini agent + argparse prompting

--- build-an-AI-agent/main.py
+++ build-an-AI-agent/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 def main():
@@ -15,9 +16,27 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+    content = args.user_prompt
+    # content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 
     response = client.models.generate_content(model='gemini-2.5-flash', contents=content)
+    
+    if response.usage_metadata == None:
+    
+        raise RuntimeError("failed API request")
+    
+    else:
+
+        prompt_tokens = f"Prompt tokens: {response.usage_metadata.prompt_token_count}"
+        response_tokens = f"Response tokens: {response.usage_metadata.candidates_token_count}"
+
+        print(prompt_tokens)
+        print(response_tokens)
 
     print(response.text)
 
